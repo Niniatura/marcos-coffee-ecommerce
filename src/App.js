fa150be5e5ec7ae87d0064866bb373ec7fff84a0
Niniatura <neli.marcos@gmail.com>
2021-10-21 23:37:52 -0300

    work in progress

--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
 import './App.css';
-import React, { useState } from "react";
-import { ItemListContainer } from './components/itemList/ItemList.js';
+import { ItemList} from './components/itemListContainer/ItemListContainer.js';
 import { NavBar } from './components/navBar/NavBar.js';
-import {Counter} from './components/itemCount/ItemCount';
+//import {ItemCount,onAdd} from './components/itemCount/ItemCount';
 
 
 function App() {
-  const [stock, setCounter] = useState (1);
-  const onAdd = () => {
-    if (stock < 5) {
-      setCounter(stock + 1)
-    }
-  }
   
-  const onDecrease = () => {
-    if (stock >= 2) { 
-      setCounter(stock - 1)
-    }
-  }
   return (
     <div className="App">
       <NavBar /> 
-      <ItemListContainer /> 
-      <Counter counter={stock} onAdd= {onAdd} onDecrease= {onDecrease} /> 
+      <ItemList /> 
+      
     </div>
     
     );
--- src/components/itemCount/ItemCount.js
+++ src/components/itemCount/ItemCount.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import './ItemCount.css';
+//import {ItemList,onAdd} from '../itemListContainer/ItemListContainer.js';
 
-export const Counter = ({counter, onDecrease, onAdd}) => {
+
+export const ItemCount = ({ stock, initial,  onAdd }) => {
+    
+    const [counter,setCounter] = useState(0);
+    
+    const onDecrese = () => {
+        if (stock <= counter) { 
+          setCounter(counter - 1)
+        }
+      }
+
+      const onIncrement = () => {
+        if (stock > counter) {
+          setCounter(counter + 1)
+        }
+      }
+
     return (
         <div className="contador">
-            <button className="btn bg-dark boton" onClick ={onDecrease}>-</button>
+            <button className="btn bg-dark boton" onClick ={onDecrese}>-</button>
             <p className="items">{counter}</p>
-            <button className="btn bg-dark boton" onClick ={onAdd}>+</button>
+            <button className="btn bg-dark boton" onClick ={onIncrement}>+</button>        
+        
         </div>
-    );
-};
-
+    )
+}
+export default ItemCount;
